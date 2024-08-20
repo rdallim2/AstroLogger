ad8be11a66fd1eeb6ffd511312f3d78f36b9d4b0
rdallim2 <ryandallimore@gmail.com>
2024-08-20 12:19:40 -0700

    working on autopopulate zip to coordinates

--- main/app.py
+++ main/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+zipDict = {}
+
+def makeZipDict(filename):
+    global zipDict
+    with open (file, 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            key = parts[0]
+            value = (int(parts[1].strip()), parts[2].strip())
+            zipDict[key] = value
+
+filename = zip-lat-lng.txt
+makeZipDict(filename)
+
 @app.route('/')
 def index():
     print("Index route was accessed.")
